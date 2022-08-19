a471b0e630229228011deb8e67f0d4f83de56af0
diamondfeng <fengdiamond@gmail.com>
2022-08-19 15:08:54 +0800

    Initialize Project

---
+++ index.js
@@ -0,0 +1,137 @@
+const BASE_URL = 'https://movie-list.alphacamp.io'
+const INDEX_URL = BASE_URL + '/api/v1/movies/'
+const POSTER_URL = BASE_URL + '/posters/'
+const MOVIES_PER_PAGE = 12
+
+const movies = []
+let filteredMovies = []
+
+const dataPanel = document.querySelector('#data-panel')
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('#search-input')
+const paginator = document.querySelector('#paginator')
+
+function renderMovieList(data) {
+  let rawHTML = ''
+
+  data.forEach((item) => {
+    //title, image
+    console.log(item)
+    rawHTML += `<div class="col-md-3 col-sm-6">
+        <div class="mb-2 mt-2">
+          <div class="card">
+            <img
+              src="${POSTER_URL + item.image}"
+              class="card-img-top" alt="Movie Poster">
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#movie-modal" data-id="${item.id}">More
+                Info</button>
+              <button class="btn btn-info text-white btn-add-favorite" data-id="${item.id}">+</button>
+            </div>
+          </div>
+        </div>
+      </div>`
+  })
+
+  dataPanel.innerHTML = rawHTML
+}
+
+function renderPaginator(amount) {
+  //計算總頁數
+  const numberOfPages = Math.ceil(amount / MOVIES_PER_PAGE)
+  //製作 template 
+  let rawHTML = ''
+
+  for (let page = 1; page <= numberOfPages; page++) {
+    rawHTML += `<li class="page-item"><a class="page-link" href="#" data-page="${page}">${page}</a></li>`
+  }
+  //放回 HTML
+  paginator.innerHTML = rawHTML
+}
+
+function getMoviesByPage(page) {
+  const data = filteredMovies.length ? filteredMovies : movies
+  const startIndex = (page - 1) * MOVIES_PER_PAGE
+
+  return data.slice(startIndex, startIndex + MOVIES_PER_PAGE)
+}
+
+function showMovieModal(id) {
+  const modalTitle = document.querySelector('#movie-modal-title')
+  const modalImage = document.querySelector('#movie-modal-image')
+  const modalDate = document.querySelector('#movie-modal-date')
+  const modalDescription = document.querySelector('#movie-modal-description')
+
+  axios.get(INDEX_URL + id).then((response) => {
+    //response.data.results
+    const data = response.data.results
+    modalTitle.innerText = data.title
+    modalDate.innerText = 'Release date: ' + data.release_date
+    modalDescription.innerText = data.description
+    modalImage.innerHTML = `<img src="${POSTER_URL + data.image}" alt="movie-poster" class="image-fluid">`
+  })
+}
+
+function addToFavorite(id) {
+  const list = JSON.parse(localStorage.getItem("favoriteMovies")) || []
+  const movie = movies.find((movie) => movie.id === id)
+
+  if (list.some((movie) => movie.id === id)) {
+    return alert('此電影已在收藏列！')
+  }
+
+  list.push(movie)
+  console.log(list)
+
+  localStorage.setItem('favoriteMovies', JSON.stringify(list))
+}
+
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.matches('.btn-show-movie')) {
+    showMovieModal(Number(event.target.dataset.id))
+  } else if (event.target.matches('.btn-add-favorite')) {
+    addToFavorite(Number(event.target.dataset.id))
+  }
+})
+
+paginator.addEventListener('click', function onPaginatorClicked(event) {
+  //如果被點擊的不是 a 標籤，結束
+  if (event.target.tagName !== 'A') return
+
+  //透過 dataset 取得被點擊的頁數
+  const page = Number(event.target.dataset.page)
+  //更新畫面
+  renderMovieList(getMoviesByPage(page))
+})
+
+searchForm.addEventListener('submit', function onSearchFormSubmitted(event) {
+  event.preventDefault()
+  const keyword = searchInput.value.trim().toLowerCase()
+
+  // if (!keyword.length) {
+  //   return alert('Please enter a valid string')
+  // }
+
+  filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(keyword)
+  )
+
+  if (filteredMovies.length === 0) {
+    return alert(`您輸入的關鍵字：${keyword} 沒有符合條件的電影`)
+  }
+  renderPaginator(filteredMovies.length)
+  renderMovieList(getMoviesByPage(1))
+})
+
+axios.get(INDEX_URL).then((response) => {
+  //Array(80)
+  movies.push(...response.data.results)
+  console.log(movies)
+  renderPaginator(movies.length)
+  renderMovieList(getMoviesByPage(1))
+})
+
+localStorage.setItem("default_language", JSON.stringify())
